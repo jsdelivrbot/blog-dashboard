d1422fc730de7bc45f0deff9367981e2e201150f
sedad kosovac <kosovacsedad@gmail.com>
2017-04-23 19:56:05 +0200

    post tests

---
+++ tests/acceptance/dashboard-frameworks-new-test.js
@@ -0,0 +1,59 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'blog-dashboard/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | dashboard frameworks new',{
+  beforeEach(){
+    visit("dashboard/frameworks/new")
+  }
+});
+
+test('visiting /dashboard-frameworks-new', function(assert) {
+  andThen(function() {
+    assert.equal(currentRouteName(), 'dashboard.frameworks.new', 'dashboard frameworks new route');
+  });
+});
+
+test('placeholders for new frameworks', function(assert) {
+  andThen(function() {
+    assert.equal(find('label').text(), 'Name', 'label for frameworks name');
+    assert.equal(find('label').text(), 'Display Order', 'label for frameworks display order');
+    // assert.equal(find('label').text(), 'secondary name', 'label for language name');
+
+  });
+});
+
+test('placeholders for new frameworks', function(assert) {
+  andThen(function() {
+    assert.equal(find('input').placeholder(), 'Name', 'input for frameworks name');
+    assert.equal(find('input').placeholder(), 'Display Order', 'input for frameworks display order');
+    // assert.equal(find('label').text(), 'secondary name', 'label for language name');
+
+  });
+});
+
+test('save button', function(assert) {
+  andThen(function() {
+     assert.equal(find('.container > .primary-button').length, 1, 'add save button is present on page');
+  });
+});
+
+test('cancel button', function(assert) {
+  andThen(function() {
+     assert.equal(find('.container > .secondary-button').length, 1, 'add cancel button is present on page');
+  });
+});
+
+
+test('frameworks save on click', function(assert) {
+  click('.primary-button');
+  andThen(function(){
+    assert.equal(currentRouteName(), 'dashboard.frameworks.show', 'frameworks route');
+  });
+});
+
+test('frameworks new cancel click', function(assert) {
+  click('.secondary-button');
+  andThen(function(){
+    assert.equal(currentRouteName(), 'dashboard.frameworks.show', 'frameworks route');
+  });
+});
--- tests/acceptance/dashboard-languages-new-test.js
+++ tests/acceptance/dashboard-languages-new-test.js
@@ -12,3 +12,49 @@ test('visiting /dashboard-languages-new', function(assert) {
     assert.equal(currentRouteName(), 'dashboard.languages.new', 'dashboard languages new route');
   });
 });
+
+test('placeholders for new language', function(assert) {
+  andThen(function() {
+    assert.equal(find('label').text(), 'Name', 'label for language name');
+    assert.equal(find('label').text(), 'Display Order', 'label for language display order');
+    assert.equal(find('label').text(), 'Theme', 'label for language theme');
+    // assert.equal(find('label').text(), 'secondary name', 'label for language name');
+
+  });
+});
+
+test('placeholders for new language', function(assert) {
+  andThen(function() {
+    assert.equal(find('input').placeholder(), 'Name', 'input for language name');
+    assert.equal(find('input').placeholder(), 'Display Order', 'input for language display order');
+    assert.equal(find('input').placeholder(), 'Theme', 'select for language theme');
+    // assert.equal(find('label').text(), 'secondary name', 'label for language name');
+
+  });
+});
+
+test('save button', function(assert) {
+  andThen(function() {
+     assert.equal(find('.container > .primary-button').length, 1, 'add save button is present on page');
+  });
+});
+
+test('cancel button', function(assert) {
+  andThen(function() {
+     assert.equal(find('.container > .secondary-button').length, 1, 'add cancel button is present on page');
+  });
+});
+
+test('languages save on click', function(assert) {
+  click('.primary-button');
+  andThen(function(){
+    assert.equal(currentRouteName(), 'dashboard.languages', 'languages route');
+  });
+});
+
+test('languages new cancel click', function(assert) {
+  click('.secondary-button');
+  andThen(function(){
+    assert.equal(currentRouteName(), 'dashboard.languages', 'language route');
+  });
+});
