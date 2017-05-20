abddc0da495a3c3610114dcf6650c8138881cf17
Sedad Kosovac <kosovacsedad@gmail.com>
2017-05-20 17:56:48 +0200

    new code format

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -29,17 +29,36 @@ export default function(server) {
     status:'header'
   });
   server.create("block", {
-    content:"<p>Block</p>",
+    content:`<p>orem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown 
+    printer took a galley of type and scrambled it to make a type specimen book. It has survived 
+    not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+    and more recently with 
+    desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>`,
     post_id:1,
     status:'block'
   });
   server.create("block", {
-    content:"<pre>Code</pre>",
+    content:`
+    <pre>
+    class Greeter
+      def initialize(name)
+        @name = name.capitalize
+      end
+
+      def salute
+        puts "Hello #{@name}!"
+      end
+    end
+   </pre>`,
     post_id:1,
     status:'code'
   });
   server.create("block", {
-    content:"<div>Hint</div>",
+    content:`<div>It is a long established fact that a reader will 
+    be distracted by the readable content of a page when looking at its layout. 
+    The point of using Lorem Ipsum is that it has a more-or-less normal distribution </div>`,
     post_id:1,
     status:'hint'
   });
