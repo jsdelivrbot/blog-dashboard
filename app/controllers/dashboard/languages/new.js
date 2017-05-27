1e86b38257ad6918b84530ed6f345ed9a790c88b
Sedad Kosovac <kosovacsedad@gmail.com>
2017-05-27 16:04:12 +0200

    dashboard and user linked, title change, slug add

--- app/controllers/dashboard/languages/new.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({  
-  session: Ember.inject.service(),
-  admin: Ember.computed("currentPath", function(){
-    return this.get("session").get("session.authenticated.admin");
-  })
-});
