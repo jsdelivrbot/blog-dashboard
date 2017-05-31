f28b4ba9bbb5f42768e8f8ae45da19f287e0783b
sedadkosovac <kosovacsedad@gmail.com>
2017-05-31 22:23:45 +0200

    highlight remove

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,5 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
   });
-  app.import('bower_components/highlightjs/highlight.pack.min.js');
-  app.import('bower_components/highlightjs/styles/github.css');
   return app.toTree();
 };
