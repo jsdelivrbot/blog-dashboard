1da7beac680965adcc01622393f7bd3c6aea6bc1
sedadkosovac <kosovacsedad@gmail.com>
2017-06-01 21:32:26 +0200

    paginate for posts on lanugage

--- app/routes/languages/show.js
+++ app/routes/languages/show.js
@@ -1,7 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+ queryParams: {
+    page: {
+      refreshModel: true
+    }
+  },
   model(params){
-    return this.store.findRecord('language', params.language_id);
+    return Ember.RSVP.hash({
+      language: this.store.findRecord("language", params.language_id),
+      posts: this.store.query("post", {
+        language_id: params.language_id,
+        page: params.page
+      }).then((results)=>{
+        return{
+          posts: results,
+          meta: results.get("meta")
+        }
+      }) 
+    })
+  },
+  setupController(controller, model){
+    controller.set("model", model.language);
+    controller.set("posts", model.posts.posts);
+    controller.set("navigate", model.posts.meta);
   }
 });
