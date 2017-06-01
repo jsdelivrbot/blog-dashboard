db569924b51d8aede62c32dd4dcb7a871a9e6958
sedadkosovac <kosovacsedad@gmail.com>
2017-06-01 22:16:48 +0200

    paginate for posts on framework

--- app/routes/frameworks/show.js
+++ app/routes/frameworks/show.js
@@ -1,7 +1,25 @@
-import Ember from 'ember';
+ import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    page: {
+      refreshModel: true
+    }
+  },
   model(params){
-    return this.store.findRecord('framework', params.framework_id);
+    console.log(params)
+    return this.store.query("post", {
+      framework_id: params.framework_id,
+      page: params.page
+    }).then((results)=>{
+      return{
+        posts: results,
+        meta: results.get("meta")
+      }
+    }) 
+  },
+  setupController(controller, {posts, meta}){
+    controller.set("posts", posts);
+    controller.set("navigate", meta);
   }
-});
+}); 
