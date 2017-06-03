a53f75108a9cd665939ec0a697731e2422f3052f
Sedad Kosovac <kosovacsedad@gmail.com>
2017-06-03 15:56:21 +0200

    pagiinate dashboard

--- app/routes/frameworks/show.js
+++ app/routes/frameworks/show.js
@@ -1,25 +1,28 @@
- import Ember from 'ember';
+import Ember from 'ember';
 
 export default Ember.Route.extend({
-  queryParams: {
+ queryParams: {
     page: {
       refreshModel: true
     }
   },
   model(params){
-    console.log(params)
-    return this.store.query("post", {
-      framework_id: params.framework_id,
-      page: params.page
-    }).then((results)=>{
-      return{
-        posts: results,
-        meta: results.get("meta")
-      }
-    }) 
+    return Ember.RSVP.hash({
+      framework: this.store.findRecord("framework", params.framework_id),
+      posts: this.store.query("post", {
+        framework_id: params.framework_id,
+        page: params.page
+      }).then((results)=>{
+        return{
+          posts: results,
+          meta: results.get("meta")
+        }
+      }) 
+    })
   },
-  setupController(controller, {posts, meta}){
-    controller.set("posts", posts);
-    controller.set("navigate", meta);
+  setupController(controller, model){
+    controller.set("model", model.framework);
+    controller.set("posts", model.posts.posts);
+    controller.set("navigate", model.posts.meta);
   }
-}); 
+});
